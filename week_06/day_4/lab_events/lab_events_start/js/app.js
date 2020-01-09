76958b64e3baadbc6a27ed2f99ec3a71b238eb08
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2020-01-09 16:19:35 +0000

    Before adding new element

---
+++ week_06/day_4/lab_events/lab_events_start/js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form')
+  form.addEventListener("submit", handleFormSubmit);
+
+  const listOfStuff = document.createElement('li');
+  listOfStuff.textContent = `Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}`;
+  listOfStuff.classList.add(list)
+
+    document.getElementById("new-item-form").reset();
+})
+
+const handleFormSubmit = function(event) {
+  event.preventDefault();
+  const resultParagraph = document.querySelector('#reading-list')
+  resultParagraph.textContent = ` Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}`;}
+ 
