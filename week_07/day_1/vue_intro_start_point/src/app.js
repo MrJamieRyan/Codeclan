dd7b0f2f5d1cd94d6687eacdf296043cbab742dc
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2020-01-13 12:01:25 +0000

    Stop for lunch

---
+++ week_07/day_1/vue_intro_start_point/src/app.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el:"#app",
+    data: {
+        greeting: "Hello World!"
+    },
+    methods: {
+      updateGreeting: function(name){
+        this.greeting = "Hello " + name
+    }
+    }
+  });
+})
